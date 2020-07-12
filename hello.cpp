89e3a842856a3ccdd8f0239c09ea165c7f3c98e1
hjzheng818 <hjzheng818@126.com>
2020-07-12 14:40:22 +0800

    测试IDEA上传文件到git

--- hello.cpp
+++ hello.cpp
@@ -6,5 +6,7 @@ void main(void)
 	printf("Hello World!\n");
 	printf("This is a test for code and pull requests.");
 	printf("This is a test at 2020-07-12 for IDEA to github")
+	printf("This is a test at 2020-07-12 for IDEA to git at 192.168.0.130")
+
 }
 
